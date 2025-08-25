1ca4fab466bb9b59d6d85b789e4967f76229f3ca
viannik <ivan.nikitski@gmail.com>
2025-08-25 23:52:44 +0300

    added racing models

--- racing/admin.py
+++ racing/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from racing.models import Manufacturer, Drone, RaceTrack
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    search_fields = ("name", "country")
+
+
+@admin.register(Drone)
+class DroneAdmin(admin.ModelAdmin):
+    list_display = (
+        "model_name",
+        "max_speed",
+        "weight",
+        "manufacturer",
+    )
+    search_fields = (
+        "model_name",
+        "manufacturer__name",
+    )
+    list_filter = ("manufacturer",)
+
+
+@admin.register(RaceTrack)
+class RaceTrackAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "difficulty_level",
+        "length_meters",
+        "location",
+    )
+    search_fields = ("name", "location")
+    list_filter = ("difficulty_level",)
--- racing/models.py
+++ racing/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+    def get_absolute_url(self):
+        return reverse(
+            "racing:manufacturer-detail",
+            kwargs={"pk": self.pk},
+        )
+
+
+class Drone(models.Model):
+    model_name = models.CharField(
+        max_length=255,
+    )
+    max_speed = models.FloatField(
+        help_text="Max speed in km/h",
+    )
+    weight = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        help_text="Weight in kg",
+    )
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="drones",
+    )
+    pilots = models.ManyToManyField(
+        "accounts.Pilot",
+        related_name="drones",
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ["model_name", "manufacturer"]
+        unique_together = ("model_name", "manufacturer")
+
+    def __str__(self):
+        return f"{self.model_name} ({self.manufacturer.name})"
+
+    def get_absolute_url(self):
+        return reverse(
+            "racing:drone-detail",
+            kwargs={"pk": self.pk},
+        )
+
+
+class RaceTrack(models.Model):
+    DIFFICULTY_CHOICES = [
+        (1, "Beginner"),
+        (2, "Intermediate"),
+        (3, "Advanced"),
+        (4, "Expert"),
+        (5, "Professional"),
+    ]
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    difficulty_level = models.IntegerField(
+        choices=DIFFICULTY_CHOICES,
+        default=1,
+    )
+    length_meters = models.IntegerField(
+        help_text="Length in meters",
+    )
+    location = models.CharField(
+        max_length=255,
+        help_text="Race track location",
+    )
+    record_time = models.DurationField(
+        null=True,
+        blank=True,
+        help_text="Record time for the track",
+    )
+
+    class Meta:
+        ordering = ["difficulty_level", "name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.get_difficulty_level_display()})"
+
+    def get_absolute_url(self):
+        return reverse(
+            "racing:racetrack-detail",
+            kwargs={"pk": self.pk},
+        )
